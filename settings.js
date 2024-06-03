8e1fc950898f2b1c856bbe7e13ca6d580d7401fa
levislel <164642225+levislel@users.noreply.github.com>
2024-06-03 05:52:59 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,18 +20,18 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '22545752448' //ur owner number
+global.ownername = '꙰𝘛𝘰𝘯𝘺 𝘔𝘰𝘯𝘵𝘢𝘯𝘢. ✘' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
+global.themeemoji = '🧟'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.packname = "Sticker By ꙰𝘛𝘰𝘯𝘺 𝘔𝘰𝘯𝘵𝘢𝘯𝘢. ✘"
+global.author = "🦄드림 가이 \n\n+22545752448"
+global.creator = "22545752448@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["22545752448"] // Premium User
 global.hituet = 0
 
 //bot sett
